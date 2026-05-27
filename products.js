e9fdcc72b8b92f3eabbf13d6844fb11a268b7188
vogo1investment-ux <vogo1investment@gmail.com>
2026-05-27 02:40:27 +0100

    Add files via upload

--- products.js
+++ products.js
@@ -2,41 +2,241 @@ const products={
 
 CM801:{
 
-img:
+name:
 
-"RTXGamingLaptop.jpg"
+"Gaming Laptop",
 
-},
+priceUsd:
 
-TV001:{
+"$9.50",
 
-img:
+priceNgn:
 
-"samsungTV.jpg"
-
-},
-
-FR200:{
+"₦15,000",
 
 img:
 
-"HaierThermocoolChestFreezer.jpg"
+"gamingLaptop.jpg",
 
-},
+desc:
 
-PS900:{
-
-img:
-
-"ps5.jpg"
+"High performance gaming laptop for gaming work streaming and editing"
 
 },
 
 IP15:{
 
+name:
+
+"iPhone 15 Pro Max",
+
+priceUsd:
+
+"$12.70",
+
+priceNgn:
+
+"₦20,000",
+
 img:
 
-"iphone15.jpg"
+"iphone15.jpg",
+
+desc:
+
+"Premium smartphone with advanced camera system and performance"
+
+},
+
+PS900:{
+
+name:
+
+"PlayStation 5",
+
+priceUsd:
+
+"$11.43",
+
+priceNgn:
+
+"₦18,000",
+
+img:
+
+"ps5console.jpg",
+
+desc:
+
+"Next generation gaming console with smooth graphics"
+
+},
+
+TV001:{
+
+name:
+
+"Samsung Smart TV",
+
+priceUsd:
+
+"$15.24",
+
+priceNgn:
+
+"₦24,000",
+
+img:
+
+"smarttv.jpg",
+
+desc:
+
+"Smart television with streaming and internet support"
+
+},
+
+FR200:{
+
+name:
+
+"Haier Thermocool Freezer",
+
+priceUsd:
+
+"$13.97",
+
+priceNgn:
+
+"₦22,000",
+
+img:
+
+"freezer.jpg",
+
+desc:
+
+"Large freezer with cooling preservation technology"
+
+},
+
+MB700:{
+
+name:
+
+"MacBook Pro",
+
+priceUsd:
+
+"$19.05",
+
+priceNgn:
+
+"₦30,000",
+
+img:
+
+"macbookpro.jpg",
+
+desc:
+
+"Professional laptop for office work editing and design"
+
+},
+
+AP500:{
+
+name:
+
+"AirPods Max",
+
+priceUsd:
+
+"$7.62",
+
+priceNgn:
+
+"₦12,000",
+
+img:
+
+"airpodsmax.jpg",
+
+desc:
+
+"Premium wireless headphones with immersive sound"
+
+},
+
+CC300:{
+
+name:
+
+"Canon Camera",
+
+priceUsd:
+
+"$10.16",
+
+priceNgn:
+
+"₦16,000",
+
+img:
+
+"canoncamera.jpg",
+
+desc:
+
+"Professional photography camera with sharp imaging"
+
+},
+
+DR100:{
+
+name:
+
+"Drone Pro",
+
+priceUsd:
+
+"$14.61",
+
+priceNgn:
+
+"₦23,000",
+
+img:
+
+"dronepro.jpg",
+
+desc:
+
+"Flying drone with built in camera system"
+
+},
+
+GN900:{
+
+name:
+
+"Generator Set",
+
+priceUsd:
+
+"$17.78",
+
+priceNgn:
+
+"₦28,000",
+
+img:
+
+"generator.jpg",
+
+desc:
+
+"Reliable power backup generator"
 
 }
 
